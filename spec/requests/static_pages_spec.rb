5f0cbf6e837aee975d9d59051e11ba59508c66b9
jaykan <Kj890821>
2013-09-10 01:11:23 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,34 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	subject { page }
 
 	describe "Home page" do
+		before { visit root_path }
 		
-		it "should have the content 'Twitterclone App'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => 'Twitterclone App')
-		end
-
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :content => "Ruby on Rails Tutorial Twitterclone App | Home ")
-		end
-
-		it "should not have a custom page title" do
-			visit '/static_pages/home'
-			page.should_not have_selector('title', :text => '| Home')
-		end
+			it { should have_selector('h1', text: 'Twitterclone App') }
+			it { should have_selector('title', content: full_title('')) }
+			it { should_not have_selector('title', text: '| Home') }
 	end 
 
 	describe "Help page" do
-		
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('h1', :text => 'Help')
-		end
+    before { visit help_path }
 
-		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('title', :content => "Ruby on Rails Tutorial Twitterclone App | Help ")
-		end
-	end
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', content: full_title('Help')) }
+  end
 
-	describe "About page" do
+  describe "About page" do
+    before { visit about_path }
 
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', :text => 'About Us')
-		end
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', content: full_title('About Us')) }
+  end
 
-		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('title', :content => "Ruby on Rails Tutorial Twitterclone App | About Us ")
-		end
-	end
+  describe "Contact page" do
+    before { visit contact_path }
 
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', content: full_title('Contact')) }
+  end
 end
-
